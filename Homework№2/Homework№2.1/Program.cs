3777b8654d93928ed055ecc3cb1248208a1fb94b
Markidon <86127865+Markidon@users.noreply.github.com>
2021-09-02 18:40:54 +0300

    Add files via upload

---
+++ Homework№2/Homework№2.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    { //Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите максимальную среднесуточную температуру");
+            int max_temp;
+            while (!int.TryParse(Console.ReadLine(), out max_temp))
+            {
+                Console.WriteLine("Веди максимальную среднесуточную температуру цифрами");
+            }
+            Console.WriteLine("Ведите минимальную среднесуточную температуру");
+            int min_temp;
+            while (!int.TryParse(Console.ReadLine(), out min_temp))
+            {
+                Console.WriteLine("Веди минимальную среднесуточную температуру цифрами");
+            }
+            Console.WriteLine("Посщитать средную температуру? если да, нажмите интер");
+            Console.WriteLine("Если нет, нажми Эскейп");
+           
+            if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                double sredn = max_temp + max_temp / 2;
+                Console.WriteLine($"Средняя температура {sredn}");
+            }
+            else if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            {
+
+            }
+
+
+        }
+    }
+}
---
+++ Homework№2/Homework№2.2/enum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_2._2
+{
+    public enum Season
+    {
+        Январь = 1,
+        Февраль,
+        Март,
+        Апрель,
+        Май,
+        Июнь,
+        Июль,
+        Август,
+        Сентябрь,
+        Октябрь,
+        Ноябрь,
+        Декабрь
+
+    }
+}
